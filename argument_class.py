a3994a6a4a7aac697d1208a7f23d7925e797ee22
Talha Malik <talha.malik@A006-01262.local>
2023-07-18 13:48:39 +0500

    Fix: argparse mutual exclusion for param validation

--- argument_class.py
+++ argument_class.py
@@ -9,10 +9,12 @@ class Argument:
     def get_command_line_arguments(self):
 
         parser = argparse.ArgumentParser()
-        parser.add_argument('-e', action = 'store_true', help = 'flag for given year highest temperature, lowest temperature, maximum humidity')
-        parser.add_argument('-a', action = 'store_true', help = 'flag for given month average highest temperature, lowest temperature, humidity')
-        parser.add_argument('-c', action = 'store_true', help = 'flag for given month two horizontal bar charts for the highest and lowest temperature on each day')
-        parser.add_argument('-d', action = 'store_true', help = 'flag for given month one horizontal bar chart for the highest and lowest temperature on each day')
+        group = parser.add_mutually_exclusive_group()
+
+        group.add_argument('-e', action = 'store_true', help = 'flag for given year highest temperature, lowest temperature, maximum humidity')
+        group.add_argument('-a', action = 'store_true', help = 'flag for given month average highest temperature, lowest temperature, humidity')
+        group.add_argument('-c', action = 'store_true', help = 'flag for given month two horizontal bar charts for the highest and lowest temperature on each day')
+        group.add_argument('-d', action = 'store_true', help = 'flag for given month one horizontal bar chart for the highest and lowest temperature on each day')
 
         parser.add_argument('date', help = 'Date argument')
         parser.add_argument('file_path', help = 'File path argument')
