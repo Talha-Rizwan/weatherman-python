b8f65e0cdbb85b1f1c59095aa827a26bb842364c
Talha Malik <talha.malik@A006-01262.local>
2023-07-18 12:40:22 +0500

    Fix: changed arguments flag description

--- argument_class.py
+++ argument_class.py
@@ -9,10 +9,10 @@ class Argument:
     def get_command_line_arguments(self):
 
         parser = argparse.ArgumentParser()
-        parser.add_argument('-e', action = 'store_true', help = 'Optional flag')
-        parser.add_argument('-a', action = 'store_true', help = 'Optional flag')
-        parser.add_argument('-c', action = 'store_true', help = 'Optional flag')
-        parser.add_argument('-d', action = 'store_true', help = 'Optional flag')
+        parser.add_argument('-e', action = 'store_true', help = 'flag for given year highest temperature, lowest temperature, maximum humidity')
+        parser.add_argument('-a', action = 'store_true', help = 'flag for given month average highest temperature, lowest temperature, humidity')
+        parser.add_argument('-c', action = 'store_true', help = 'flag for given month two horizontal bar charts for the highest and lowest temperature on each day')
+        parser.add_argument('-d', action = 'store_true', help = 'flag for given month one horizontal bar chart for the highest and lowest temperature on each day')
 
         parser.add_argument('date', help = 'Date argument')
         parser.add_argument('file_path', help = 'File path argument')
